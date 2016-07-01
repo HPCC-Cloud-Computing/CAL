40e2ca1049b8b6f363357024e0f510c16402be02
Binh-Minh Nguyen <minhnb@soict.hust.edu.vn>
2016-07-02 04:23:37 +0700

    xong stevedore

--- cal/v1/__init__.py
+++ cal/v1/__init__.py
@@ -1,25 +1,42 @@
-from cal import wsgi
+import cal.wsgi
 from routes import Mapper
 
-from .resource_extensions.basic import Basics
+from cal.v1.resource_extensions.basic import Basics
+import stevedore
 
-
-class APIRouterV1(wsgi.Router):
+class APIRouterV1(cal.wsgi.Router):
     """
     Techbk:
     - Class nay co nhiem vu load extension Resource.
     - No khoi tao mot object Mapper
     """
 
+    @staticmethod
+    def api_extension_namespace():
+        return 'cal.v1.resource_extensions'
+
     def __init__(self):
+
+        def _check_load_extension(ext):
+            return True
+
+        self.api_extension_manager = stevedore.enabled.EnabledExtensionManager(
+            namespace=self.api_extension_namespace(),
+            check_func=_check_load_extension,
+            invoke_on_load=True
+        )
+
+
+
         mapper = Mapper()
-        #Dang ky controller o day
-        resources = [Basics(),]
-        for resource in resources:
+        # #Dang ky controller o day
+        # resources = [Basics(),]
+        for ext in self.api_extension_manager:
+            resource = ext.obj
             mapper.resource(
                 resource.member_name,
                 resource.collection_name,
-                controller=wsgi.Resource(resource.controller),
+                controller=cal.wsgi.Resource(resource.controller),
                 member=resource.member,
                 collection=resource.collection)
 
