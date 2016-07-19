3b339a9c27196289094fe243fcec41a64d1b69fe
Kien Nguyen <ntk148v@gmail.com>
2016-07-19 18:28:36 +0700

    Init coverage test

--- cal/conf/network.py
+++ cal/conf/network.py
@@ -4,13 +4,15 @@ from oslo_config import cfg
 network_group = cfg.OptGroup('network',
                              title='Network Options')
 
-# some config options here 
+# some config options here
 
 ALL_OPTS = ()
+
 
 def register_opts(conf):
     conf.register_group(network_group)
     conf.register_opts(ALL_OPTS, group=network_group)
 
+
 def list_opts():
-    return {network_group: ALL_OPTS} 
+    return {network_group: ALL_OPTS}
