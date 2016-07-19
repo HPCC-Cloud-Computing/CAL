af2374d94601cbaa596a00b7d24c4b2563575992
Kien Nguyen <ntk148v@gmail.com>
2016-07-19 15:49:19 +0700

    Init network config

--- cal/conf/__init__.py
+++ cal/conf/__init__.py
@@ -1,6 +1,10 @@
 from oslo_config import cfg
+
+from cal.conf import network
 from cal.conf import wsgi
 
 CONF = cfg.CONF
 
+network.register_opts(CONF)
 wsgi.register_opts(CONF)
+
---
+++ cal/conf/network.py
@@ -0,0 +1,16 @@
+from oslo_config import cfg
+
+
+network_group = cfg.OptGroup('network',
+                             title='Network Options')
+
+# some config options here 
+
+ALL_OPTS = ()
+
+def register_opts(conf):
+    conf.register_opts(network_group)
+    conf.register_opts(ALL_OPTS, group=network_group)
+
+def list_opts():
+    return {network_group: ALL_OPTS} 
