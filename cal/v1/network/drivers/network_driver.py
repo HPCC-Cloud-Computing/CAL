9089ef0b63b6989f43358f48389ab78beae3f2dc
daikk115 <daikk115@gmail.com>
2016-08-12 00:17:28 +0700

    Add QuotaAbstact Class and init test case

--- cal/v1/network/drivers/network_driver.py
+++ cal/v1/network/drivers/network_driver.py
@@ -1,7 +1,5 @@
-""" this is contain abstract Class
-for all network driver which we want to implement
-"""
-"""NetworkDriver abstract
+""" this is contain Abstract Class and Quota Class
+    for all network driver which we want to implement.
 """
 
 
@@ -12,6 +10,7 @@ class NetworkDriver(object):
     def __init__(self):
         super(NetworkDriver, self).__init__()
         self.provider = "Unknown"
+        self.network_quota = NetworkQuota()
 
     def create():
         raise NotImplementedError
@@ -27,3 +26,52 @@ class NetworkDriver(object):
 
     def delete():
         raise NotImplementedError
+
+
+class NetworkQuota(object):
+    """docstring for QuotaNetwork"""
+    def __init__(self):
+        super(NetworkQuota, self).__init__()
+        self.get()
+
+    def get(self):
+        """Get quota from Cloud Provider."""
+
+        # get all network quota from Cloud Provider.
+        attrs = ("networks",
+                "security_groups",
+                "floating_ips",
+                "routers",
+                "internet_gateways",
+                "vpn_gateways",
+                "firewall")
+
+        for attr in attrs:
+            setattr(self, attr, eval("self.get_{}()". format(attr)))
+
+    def set(self, attr, value):
+        """Update quota for Network."""
+
+        # set special attribute.
+        setattr(self, attr, value)
+
+    def get_networks(self):
+        return None
+
+    def get_security_groups(self):
+        return None
+
+    def get_floating_ips(self):
+        return None
+
+    def get_routers(self):
+        return None
+
+    def get_internet_gateways(self):
+        return None
+
+    def get_vpn_gateways(self):
+        return None
+
+    def get_firewall(self):
+        return None
