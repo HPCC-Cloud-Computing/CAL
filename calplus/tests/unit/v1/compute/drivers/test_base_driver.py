eeb1383b1e62be462a16df6465473c7ed43e7828
daikk115 <daikk115@gmail.com>
2016-11-05 03:39:57 +0700

    Add compute base driver

---
+++ calplus/tests/unit/v1/compute/drivers/test_base_driver.py
@@ -0,0 +1,114 @@
+""" OpenstackDriver for Network
+    based on NetworkDriver
+"""
+
+
+from calplus.tests import base
+from calplus.v1.compute.drivers.base import BaseDriver
+
+
+class FakeDriver(BaseDriver):
+    """docstring for FakeDriver
+        This class implemented all of BaseDriver functions
+    """
+    def __init__(self):
+        super(FakeDriver, self).__init__()
+
+    def create(self, image_id, flavor_id,
+               network_id, name=None, number=1, **kargs):
+        return 'fake_result'
+
+    def show(self, instance_id):
+        return 'fake_result'
+
+    def list(self, **search_opts):
+        return 'fake_result'
+
+    def delete(self, instance_id):
+        return 'fake_result'
+
+    def shutdown(self, instance_id):
+        return 'fake_result'
+
+    def start(self, instance_id):
+        return 'fake_result'
+
+    def reboot(self, instance_id):
+        return 'fake_result'
+
+    def resize(self, instance_id, configuration):
+        return 'fake_result'
+
+    def add_sg(self, instance_id, new_sg):
+        """Add a security group"""
+        return 'fake_result'
+
+    def delete_sg(self, instance_id, new_sg):
+        """Delete a security group"""
+        return 'fake_result'
+
+    def list_sg(self, instance_id):
+        """List all security group"""
+        return 'fake_result'
+
+    def add_nic(self, instance_id, new_sg):
+        """Add a Network Interface Controller"""
+        return 'fake_result'
+
+    def delete_nic(self, instance_id, new_sg):
+        """Delete a Network Interface Controller"""
+        return 'fake_result'
+
+    def list_nic(self, instance_id):
+        """List all Network Interface Controller"""
+        return 'fake_result'
+
+    def add_private_ip(self, instance_id, new_sg):
+        """Add private IP"""
+        return 'fake_result'
+
+    def delete_private_ip(self, instance_id, new_sg):
+        """Delete private IP"""
+        return 'fake_result'
+
+    def add_public_ip(self, instance_id, new_sg):
+        """Add a external IP"""
+        return 'fake_result'
+
+    def delete_public_ip(self, instance_id, new_sg):
+        """Delete a external IP"""
+        return 'fake_result'
+
+    def list_ip(self, instance_id, new_sg):
+        """Add all IPs"""
+        return 'fake_result'
+
+
+class FakeDriverError(BaseDriver):
+    """docstring for FakeDriverError
+        This class didn't implement some functions
+    """
+    def __init__(self):
+        super(FakeDriverError, self).__init__()
+
+    def create(self, image_id, flavor_id,
+               network_id, name=None, number=1, **kargs):
+        return 'fake_result'
+
+    def show(self, instance_id):
+        return 'fake_result'
+
+
+class ComputeDriverTest(base.TestCase):
+
+    """Testing class for BaseDriver"""
+
+    def setUp(self):
+        super(ComputeDriverTest, self).setUp()
+
+    def test_create_base_driver_object(self):
+        fake_driver = FakeDriver()
+        self.assertIsInstance(fake_driver, FakeDriver)
+
+    def test_create_base_driver_object_unable_to_create(self):
+        self.assertRaises(TypeError, FakeDriverError)
--- calplus/v1/compute/drivers/base.py
+++ calplus/v1/compute/drivers/base.py
@@ -1,9 +1,135 @@
+""" this is contain Abstract Class and Quota Class
+    for all network driver which we want to implement.
+"""
+
 import abc
 import six
 
 
 @six.add_metaclass(abc.ABCMeta)
 class BaseDriver(object):
+    """abstract class for compute driver"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
+        super(BaseDriver, self).__init__()
+
+    @abc.abstractmethod
+    def create(self, image, flavor,
+               network_id, name, number, **kargs):
+        pass
+
+    @abc.abstractmethod
+    def show(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def list(self, **search_opts):
+        pass
+
+    @abc.abstractmethod
+    def delete(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def shutdown(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def start(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def reboot(self, instance_id):
+        pass
+
+    @abc.abstractmethod
+    def resize(self, instance_id, configuration):
+        pass
+
+    @abc.abstractmethod
+    def add_sg(self, instance_id, new_sg):
+        """Add a security group"""
+        pass
+
+    @abc.abstractmethod
+    def delete_sg(self, instance_id, new_sg):
+        """Delete a security group"""
+        pass
+
+    @abc.abstractmethod
+    def list_sg(self, instance_id):
+        """List all security group"""
+        pass
+
+    @abc.abstractmethod
+    def add_nic(self, instance_id, new_sg):
+        """Add a Network Interface Controller"""
+        pass
+
+    @abc.abstractmethod
+    def delete_nic(self, instance_id, new_sg):
+        """Delete a Network Interface Controller"""
+        pass
+
+    @abc.abstractmethod
+    def list_nic(self, instance_id):
+        """List all Network Interface Controller"""
+        pass
+
+    @abc.abstractmethod
+    def add_private_ip(self, instance_id, new_sg):
+        """Add private IP"""
+        pass
+
+    @abc.abstractmethod
+    def delete_private_ip(self, instance_id, new_sg):
+        """Delete private IP"""
+        pass
+
+    @abc.abstractmethod
+    def add_public_ip(self, instance_id, new_sg):
+        """Add a external IP"""
+        pass
+
+    @abc.abstractmethod
+    def delete_public_ip(self, instance_id, new_sg):
+        """Delete a external IP"""
+        pass
+
+    @abc.abstractmethod
+    def list_ip(self, instance_id, new_sg):
+        """Add all IPs"""
+        pass
+
+
+class BaseQuota(object):
+    """docstring for QuotaNetwork"""
+
+    def __init__(self):
+        super(BaseQuota, self).__init__()
+
+    def get(self):
+        """Get quota from Cloud Provider."""
+
+        # get all network quota from Cloud Provider.
+        attrs = ("vcpus",
+                 "instances",
+                 "ram")
+
+        for attr in attrs:
+            setattr(self, attr, eval("self.get_{}()".format(attr)))
+
+    def set(self, attr, value):
+        """Update quota for Network."""
+
+        # set special attribute.
+        setattr(self, attr, value)
+
+    def get_vcpus(self):
+        pass
+
+    def get_instances(self):
+        pass
+
+    def get_ram(self):
         pass
