cacea31501ec6efe1cd221f9e47be9a7371b5b17
daikk115 <daikk115@gmail.com>
2016-11-10 23:19:29 +0700

    Update OPS compute driver: add testing for create function - P2

--- calplus/tests/unit/v1/compute/drivers/test_openstack_driver.py
+++ calplus/tests/unit/v1/compute/drivers/test_openstack_driver.py
@@ -100,3 +100,131 @@ class OpenstackDriverTest(base.TestCase):
                 flavor='fake_flavor_id',
                 nics=[{'net-id': 'fake_net_id'}]
             )
+
+    def test_show_successfully(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'get',
+            mock.Mock(return_value=mock.Mock))
+        # NOTE: in fact: mock.Mock is novaclient.v2.servers.Server
+
+        self.fake_driver.show('fake_id')
+
+        self.fake_driver.client.servers.get. \
+            assert_called_once_with('fake_id')
+
+    def test_show_unable_to_show(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'get',
+            mock.Mock(side_effect=ClientException))
+
+        self.assertRaises(ClientException,
+            self.fake_driver.show, 'fake_id')
+
+        self.fake_driver.client.servers.get. \
+            assert_called_once_with('fake_id')
+
+    def test_list_successfully(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'list',
+            mock.Mock(return_value=[mock.Mock, mock.Mock]))
+        # NOTE: in fact: return_value is novaclient.base.ListWithMeta
+
+        self.fake_driver.list()
+
+        self.fake_driver.client.servers.list. \
+            assert_called_once_with()
+
+    def test_list_unable_to_list(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'list',
+            mock.Mock(side_effect=ClientException))
+
+        self.assertRaises(ClientException,
+            self.fake_driver.list)
+
+        self.fake_driver.client.servers.list. \
+            assert_called_once_with()
+
+    def test_delete_successfully(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'delete',
+            mock.Mock(return_value=True))
+
+        self.fake_driver.delete('fake_id')
+
+        self.fake_driver.client.servers.delete. \
+            assert_called_once_with('fake_id')
+
+    def test_delete_unable_to_delete(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'delete',
+            mock.Mock(side_effect=ClientException))
+
+        self.assertRaises(ClientException,
+            self.fake_driver.delete, 'fake_id')
+
+        self.fake_driver.client.servers.delete. \
+            assert_called_once_with('fake_id')
+
+    def test_shutdown_successfully(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'stop',
+            mock.Mock(return_value=True))
+
+        self.fake_driver.shutdown('fake_id')
+
+        self.fake_driver.client.servers.stop. \
+            assert_called_once_with('fake_id')
+
+    def test_shutdown_unable_to_shutdown(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'stop',
+            mock.Mock(side_effect=ClientException))
+
+        self.assertRaises(ClientException,
+            self.fake_driver.shutdown, 'fake_id')
+
+        self.fake_driver.client.servers.stop. \
+            assert_called_once_with('fake_id')
+
+    def test_start_successfully(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'start',
+            mock.Mock(return_value=True))
+
+        self.fake_driver.start('fake_id')
+
+        self.fake_driver.client.servers.start. \
+            assert_called_once_with('fake_id')
+
+    def test_start_unable_to_start(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'start',
+            mock.Mock(side_effect=ClientException))
+
+        self.assertRaises(ClientException,
+            self.fake_driver.start, 'fake_id')
+
+        self.fake_driver.client.servers.start. \
+            assert_called_once_with('fake_id')
+
+    def test_reboot_successfully(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'reboot',
+            mock.Mock(return_value=True))
+
+        self.fake_driver.reboot('fake_id')
+
+        self.fake_driver.client.servers.reboot. \
+            assert_called_once_with('fake_id')
+
+    def test_reboot_unable_to_reboot(self):
+        self.mock_object(
+            self.fake_driver.client.servers, 'reboot',
+            mock.Mock(side_effect=ClientException))
+
+        self.assertRaises(ClientException,
+            self.fake_driver.reboot, 'fake_id')
+
+        self.fake_driver.client.servers.reboot. \
+            assert_called_once_with('fake_id')
